232dab9cd02f76e73c116454965e8428b3f9e377
yaourtpourtoi <oleg.filatov@phystech.edu>
2021-03-11 18:17:53 +0100

    handling corrupted files

--- scaling.py
+++ scaling.py
@@ -43,28 +43,32 @@ def compute_std(sums, sums2, counts, aggregate=True, *file_range):
 
 def compute_scaling(file_name, file_i, tree_name, scaling_dict, grid_selection_dict, sums, sums2, counts, means_stds, log_step, version):
     with uproot.open(file_name, array_cache='5 GB') as f:
-        for var_type, var_dict in scaling_dict.items():
-            for var, (selection, aliases) in var_dict.items():
-                for grid_type, grid_cut in grid_selection_dict[var_type].items():
-                    if selection == None:
-                        cut = grid_cut
-                    elif grid_cut == None:
-                        cut = None
-                    else:
-                        cut = f'{selection} & {grid_cut}'
-                    taus_batches = f[tree_name].iterate(var, cut=cut, aliases=aliases, step_size='1 GB') # , how='zip'
-                    for batch in taus_batches:
-                        sums[var_type][var][grid_type][file_i] += ak.sum(batch[var])
-                        sums2[var_type][var][grid_type][file_i] += ak.sum(batch[var]**2)
-                        counts[var_type][var][grid_type][file_i] += ak.count(batch[var])
-                        del(batch)
-                    if file_i%log_step == 0:
-                        mean = compute_mean(sums[var_type][var][grid_type], counts[var_type][var][grid_type], aggregate=True)
-                        std = compute_std(sums[var_type][var][grid_type], sums2[var_type][var][grid_type], counts[var_type][var][grid_type], aggregate=True)
-                        means_stds[var_type][var][grid_type] = {'mean': mean, 'std': std}
-    if file_i%log_step == 0:
-        with open(f'output/means_stds_v{version}_log_{file_i//log_step}.json', 'w') as fout:
-            json.dump(means_stds, fout)
+        if len(f.keys()) == 0:
+            return 0
+        else:
+            for var_type, var_dict in scaling_dict.items():
+                for var, (selection, aliases) in var_dict.items():
+                    for grid_type, grid_cut in grid_selection_dict[var_type].items():
+                        if selection == None:
+                            cut = grid_cut
+                        elif grid_cut == None:
+                            cut = None
+                        else:
+                            cut = f'{selection} & {grid_cut}'
+                        taus_batches = f[tree_name].iterate(var, cut=cut, aliases=aliases, step_size='1 GB') # , how='zip'
+                        for batch in taus_batches:
+                            sums[var_type][var][grid_type][file_i] += ak.sum(batch[var])
+                            sums2[var_type][var][grid_type][file_i] += ak.sum(batch[var]**2)
+                            counts[var_type][var][grid_type][file_i] += ak.count(batch[var])
+                            del(batch)
+                        if file_i%log_step == 0:
+                            mean = compute_mean(sums[var_type][var][grid_type], counts[var_type][var][grid_type], aggregate=True)
+                            std = compute_std(sums[var_type][var][grid_type], sums2[var_type][var][grid_type], counts[var_type][var][grid_type], aggregate=True)
+                            means_stds[var_type][var][grid_type] = {'mean': mean, 'std': std}
+            if file_i%log_step == 0:
+                with open(f'output/means_stds_v{version}_log_{file_i//log_step}.json', 'w') as fout:
+                    json.dump(means_stds, fout)
+            return 1
 
 
 ########################################################################
@@ -85,7 +89,7 @@ if __name__ == '__main__':
     assert len(file_range)==2 and file_range[0]<=file_range[1]
     file_names = sorted(glob(file_path))[file_range[0]:file_range[1]]
     print(f'\n[INFO] will process {len(file_names)} input files from {file_path}')
-    print(f'[INFO] will dump means & stds to json after every {log_step} files:\n')
+    print(f'[INFO] will dump means & stds to json after every {log_step} files')
 
     # initialize sums and counts
     sums, sums2, counts, means_stds = nested_dict(), nested_dict(), nested_dict(), nested_dict()
@@ -95,22 +99,32 @@ if __name__ == '__main__':
                 sums[var_type][var][grid_type] = np.zeros(len(file_names), dtype='float64')
                 sums2[var_type][var][grid_type] = np.zeros(len(file_names), dtype='float64')
                 counts[var_type][var][grid_type] = np.zeros(len(file_names), dtype='int64')
-
-    # calculate sums & counts
     print('[INFO] starting to accumulate sums & counts:\n')
+    skip_counter = 0
     program_starts = time.time()
     last_file_done = program_starts
+    # loop over input files
     for file_i, file_name in enumerate(file_names):
-        compute_scaling(file_name, file_i, tree_name, scaling_dict, grid_selection_dict, sums, sums2, counts, means_stds, log_step, version)
+        scaling_status = compute_scaling(file_name, file_i, tree_name, scaling_dict, grid_selection_dict, sums, sums2, counts, means_stds, log_step, version)
         gc.collect()
-        processed_file = time.time()
-        print(f'---> processed {file_name} in {processed_file - last_file_done:.2f} s')
-        last_file_done = processed_file
+        if scaling_status == 0: # there is no objects in the file
+            print(f'[WARNING] couldn\'t find any object in {file_name}: skipping the file')
+            skip_counter += 1
+            last_file_done = time.time()
+            continue
+        else:
+            processed_file = time.time()
+            print(f'---> processed {file_name} in {processed_file - last_file_done:.2f} s')
+            last_file_done = processed_file
+    # calculate scaling parameters from the final sums/sums2/counts dicts
     for var_type, var_dict in scaling_dict.items():
         for var in var_dict.keys():
             for grid_type in grid_selection_dict[var_type].keys():
                 mean = compute_mean(sums[var_type][var][grid_type], counts[var_type][var][grid_type], aggregate=True)
                 std = compute_std(sums[var_type][var][grid_type], sums2[var_type][var][grid_type], counts[var_type][var][grid_type], aggregate=True)
                 means_stds[var_type][var][grid_type] = {'mean': mean, 'std': std}
+    # save to json
     with open(f'output/means_stds_v{version}.json', 'w') as fout:
         json.dump(means_stds, fout)
+        print(f'saved the scaling parameters to: output/means_stds_v{version}.json')
+    print(f'[WARNING] during the processing {skip_counter} files with no objects were skipped ')
